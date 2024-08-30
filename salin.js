4708c5994866c7f5a3a4070d9c52b7b42fd76fd0
nkolis <nurkholis010@gmail.com>
2024-08-30 11:08:36 +0700

    update

--- salin.js
+++ salin.js
@@ -1,9 +1,6 @@
-function generateCodeList() {
-  const codeInput = document.getElementById('codeInput');
-  const codes = codeInput.value.split('\n');
+function updateUICodes(codes) {
   const codeListDiv = document.getElementById('codeList');
   codeListDiv.innerHTML = ''; // Clear previous content
-
   codes.forEach((code, index) => {
     if (code.trim() !== '') {
 
@@ -21,7 +18,41 @@ function generateCodeList() {
   });
 }
 
+
+function generateCodeList() {
+  const codeInput = document.getElementById('codeInput');
+  const codes = codeInput.value.split('\n');
+  codes.sort();
+  localStorage.removeItem('codes');
+  localStorage.setItem('codes', JSON.stringify(codes))
+  updateUICodes(codes);
+}
+
 function resetCodeList() {
   document.getElementById('codeInput').value = '';
 }
+
+const filterElementTemplate = `
+      <select id='game-filter' class="mt-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500">
+      <option value='all'> Tampilkan semua</option>
+      ${gameKeys.map(item => {
+  return `<option value='${item}'> ${item}</option>`;
+})}
+      </select>
+  `;
+
+const filterEl = document.querySelector('#filter');
+const div = document.createElement('div');
+div.innerHTML = filterElementTemplate;
+filterEl.append(div);
+
+document.querySelector('#game-filter').addEventListener('change', (item) => {
+  const game = item.target.value;
+  const codes = JSON.parse(localStorage.getItem('codes'));
+  const codeFiltered = codes.filter(item => item.includes(game));
+  updateUICodes(codeFiltered);
+  if (game === 'all') {
+    updateUICodes(codes);
+  }
+})
 
